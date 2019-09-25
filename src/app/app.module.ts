716317c9257e19159c7465e90169bfb43b66ba51
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-09-25 10:15:37 -0500

    add spartacus modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://localhost:9002',
+          prefix: '/rest/v2/',
+          legacy: false,
+        },
+      },
+      authentication: {
+        client_id: 'mobile_android',
+        client_secret: 'secret',
+      },
+     context: {
+        urlParameters: ['baseSite', 'language', 'currency'],
+        baseSite: [
+           'belcorp-pe',
+          'belcorp-cl',
+        ],
+         currency : ['PEN','CLP']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
