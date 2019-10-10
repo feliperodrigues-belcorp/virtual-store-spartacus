5c6db57d53472b20b2d707187306bc85001982a2
Victor Marques2 <Victor.Marques2@keyruscorp.key>
2019-10-10 15:52:05 -0300

    request Ubigeo

--- src/app/belcorp/components/belcorp-search-consultant/belcorp-search-consultant.component.ts
+++ src/app/belcorp/components/belcorp-search-consultant/belcorp-search-consultant.component.ts
@@ -1,46 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { SearchConsultant } from '../../core/src/model/belcorp-search-consultant.model';
 import { SearchConsultantService } from '../../core/src/services/belcorp-search-consultant.service';
-import { Observable } from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-belcorp-search-consultant',
   templateUrl: './belcorp-search-consultant.component.html',
   styleUrls: ['./belcorp-search-consultant.component.scss'],
-  providers: [ SearchConsultantService ]
+  providers: [SearchConsultantService]
 })
 
 export class SearchConsultantComponent implements OnInit {
+  public forState: any;
   public searchConsultant: Observable<SearchConsultant[]>;
   searchConsultantFormBasicsInformations: FormGroup;
   options: string[] = ['One', 'Two', 'Three'];
   filteredOptions: Observable<string[]>;
   myControl = new FormControl();
 
+  constructor(private searchConsultantService: SearchConsultantService, private formBuilder: FormBuilder) {
+    this.searchConsultantService = searchConsultantService;
+  }
+
   ngOnInit() {
+    this.getUbigeo();
     this.searchConsultantFormBasicsInformations = this.formBuilder.group({
-        firstname: ['', Validators.required],
-        lastname: ['', Validators.required],
-        distrito: ['', Validators.required]
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      distrito: ['', Validators.required]
     });
 
     this.filteredOptions = this.myControl.valueChanges
       .pipe(
         startWith(''),
         map(value => this._filter(value))
-    );
+      );
+  }
+
+  private getUbigeo() {
+    this.searchConsultantService.getUbigeo().subscribe((data) => {
+      console.log(data);
+      this.forState = data;
+    });
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
-  constructor(private searchConsultantService: SearchConsultantService, private formBuilder: FormBuilder) {
-    this.searchConsultantService = searchConsultantService;
   }
   public submit() {
     this.searchConsultant = this.searchConsultantService.getSearchConsultant(this.searchConsultantFormBasicsInformations.controls);
--- src/app/belcorp/core/src/services/belcorp-search-consultant.service.ts
+++ src/app/belcorp/core/src/services/belcorp-search-consultant.service.ts
@@ -1,20 +1,26 @@
-import { HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { SearchConsultant, SearchUbigeo } from '../model/belcorp-search-consultant.model';
 
 
 @Injectable()
 export class SearchConsultantService {
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     public getSearchConsultant(form: any): Observable<SearchConsultant[]> {
         const params = new HttpParams().set('_page', form.firstname.value).set('_limit', form.lastname.value);
-        return this.http.get<SearchConsultant[]>('http://fakerestapi.azurewebsites.net/api/Users', {params} );
+        return this.http.get<SearchConsultant[]>('http://fakerestapi.azurewebsites.net/api/Users', { params });
     }
 
     public getUbigeo(): Observable<SearchUbigeo[]> {
-        return this.http.get<SearchUbigeo[]>('http://fakerestapi.azurewebsites.net/api/Users' );
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAyMywidXNyIjoiYXBwVlMiLCJpYXQiOjE1NzA3MzE0OTUsImV4cCI6MTU3MDgxNzg5NX0.VlVv8RGs_rIBN9CP0R05Om4LyoxMGrY874PXo9s5yPk';
+        const header = new HttpHeaders({ 'Content-Type': 'application', 'x-access-token': token });
+        return this.http.get<SearchUbigeo[]>('https://api-qa.belcorp.biz/countries/PE?show_ubigeo=true', { headers: header })
+            .pipe(map(response => {
+                return response
+            }));
     }
 }
