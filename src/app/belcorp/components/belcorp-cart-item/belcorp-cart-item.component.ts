760ad3e21d3af2cdaa165b7b93a39957443dea7d
matheus harsani <matheusharsani@matheuss-MacBook-Pro.local>
2019-10-16 19:39:09 -0300

    fix problem with cart

--- src/app/belcorp/components/belcorp-cart-item/belcorp-cart-item.component.ts
+++ src/app/belcorp/components/belcorp-cart-item/belcorp-cart-item.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
-import { CartItemComponent } from '@spartacus/storefront';
+import { Component, Input } from '@angular/core';
+import { CartItemComponent, Item } from '@spartacus/storefront';
 
 @Component({
   selector: 'app-belcorp-cart-item',
   templateUrl: './belcorp-cart-item.component.html',
-  styleUrls: ['./belcorp-cart-item.component.scss']
+  styleUrls: ['./belcorp-cart-item.component.scss'],
 })
 export class BelcorpCartItemComponent extends CartItemComponent {
-
   constructor() {
-    super()
+    super();
   }
+  @Input()
+  item: ItemBelcorp;
 
+  getMinNumber(x: any, y: any): any {
+    return Math.min(x, y);
+  }
+}
 
-
+interface ItemBelcorp extends Item {
+  actualQuantity?: any;
 }
