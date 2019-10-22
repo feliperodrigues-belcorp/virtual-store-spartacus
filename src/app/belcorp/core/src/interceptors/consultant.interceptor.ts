c70c384ed26ae8f33cae813ff808381ce67d245e
matheus harsani <matheusharsani@matheuss-MacBook-Pro.local>
2019-10-22 18:46:30 -0300

    fix interceptor

--- src/app/belcorp/core/src/interceptors/consultant.interceptor.ts
+++ src/app/belcorp/core/src/interceptors/consultant.interceptor.ts
@@ -1,17 +1,22 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 @Injectable()
 export class ConsultantInterceptor implements HttpInterceptor {
   public cookieValueConsultant;
   public cookieValueConsultantIso;
-  constructor(private cookieService: CookieService) {}
+  public navigateUrl;
+  constructor(private cookieService: CookieService, private router: Router) {}
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.cookieValueConsultant = this.cookieService.get('consultant_id');
     this.cookieValueConsultantIso = this.cookieService.get('consultant_iso');
+    const str = this.router.url;
+    const n = str.lastIndexOf('/');
+    const result = str.substring(n + 1);
 
-    if (this.cookieValueConsultant.length > 0) {
+    if (this.cookieValueConsultant.length > 0 && result !== 'search') {
       request = request.clone({
         setHeaders: {
           consultant_id: this.cookieValueConsultant,
